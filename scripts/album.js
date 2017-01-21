fce195ed0af9aae1d1f64558cd7df290e6c860bb
Remington Stone <remstone7@gmail.com>
2017-01-21 11:17:38 -0500

    checkpoint 13 final

--- scripts/album.js
+++ scripts/album.js
@@ -44,28 +44,30 @@ var createSongRow = function(songNumber, songName, songLength){
 				return template;
 }
 
+// set the current album
 var setCurrentAlbum = function(album){
-	// select html elements 
-	var albumTitle = document.getElementsByClassName('album-view-title')[0];
-	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-	var albumImage = document.getElementsByClassName('album-cover-art')[0];
-	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+	
+    // select html elements 
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 	
 	
-	//first child identifies child node
-	//node value returns or sets the value of node
-	albumTitle.firstChild.nodeValue = album.title;
-	albumArtist.firstChild.nodeValue = album.artist;
-	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-	albumImage.setAttribute('src', album.albumArtUrl);
+	  //first child identifies child node
+	  //node value returns or sets the value of node
+	  albumTitle.firstChild.nodeValue = album.title;
+	  albumArtist.firstChild.nodeValue = album.artist;
+	  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+	  albumImage.setAttribute('src', album.albumArtUrl);
 	
-	// clear album song list
-	albumSongList.innerHTML = '';
+	  // clear album song list
+	  albumSongList.innerHTML = '';
 	
-	// go through all from album and insert into html
-	for(var i = 0; i < album.songs.length;i++){
-		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+	  // go through all from album and insert into html
+	  for(var i = 0; i < album.songs.length;i++){
+	  	albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };
 
@@ -156,7 +158,7 @@ window.onload = function(){
         var songItem = getSongItem(event.target);
         
         // if not currently playing, pause it
-        if (songItem.getAttribute('data-song-number') !== currentlyPlaying){
+        if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong){
             songItem.innerHTML = pauseButtonTemplate;
         }
 		}
